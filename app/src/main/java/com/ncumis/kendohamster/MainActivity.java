7b84e129d8e8d3e64e8c049b11a7187cd3385db8
Anita <a0980136146@gmail.com>
2022-04-19 20:35:12 +0800

    MainActivity

--- app/src/main/java/com/ncumis/kendohamster/MainActivity.java
+++ app/src/main/java/com/ncumis/kendohamster/MainActivity.java
@@ -2,13 +2,51 @@ package com.ncumis.kendohamster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnMotionList,btnHistory,btnTrainingMenu,btnSettings;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnMotionList = findViewById(R.id.btnMotionList);
+        btnHistory = findViewById(R.id.btnHistory);
+        btnTrainingMenu =findViewById(R.id.btnTrainingMenu);
+        btnSettings = findViewById(R.id.btnSettings);
+
+        btnMotionList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,MotionList.class);
+                startActivity(i);
+            }
+        });
+        btnTrainingMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,TrainingMenu.class);
+                startActivity(i);
+            }
+        });
+        btnHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,History.class);
+                startActivity(i);
+            }
+        });
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,Settings.class);
+                startActivity(i);
+            }
+        });
     }
 }
