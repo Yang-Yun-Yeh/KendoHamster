c99819a760dd6a9a6067ff1d612c610b82b40d46
Anita <a0980136146@gmail.com>
2022-04-23 17:46:49 +0800

    finish settings history menu layout

--- app/src/main/java/com/ncumis/kendohamster/TrainingMenu.java
+++ app/src/main/java/com/ncumis/kendohamster/TrainingMenu.java
@@ -3,12 +3,44 @@ package com.ncumis.kendohamster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class TrainingMenu extends AppCompatActivity {
+    ListView listView;
+    String menu[];
+    ArrayAdapter<String> adapter;
 
+    Button buttonAdd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_training_menu);
+
+        listView = findViewById(R.id.listGeneral);
+        buttonAdd = findViewById(R.id.buttonAddMenu);
+        menu = getResources().getStringArray(R.array.menu);
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,menu);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String menu = adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(getApplicationContext(),"選擇" + menu,Toast.LENGTH_LONG).show();
+            }
+        });
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
