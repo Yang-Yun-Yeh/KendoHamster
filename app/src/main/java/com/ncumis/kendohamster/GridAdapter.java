08a41c802ea3a4ec34cd23c06e3fb1e676ce652e
puff <judy900401@gmail.com>
2022-04-23 17:45:16 +0800

    finish motionList layout

---
+++ app/src/main/java/com/ncumis/kendohamster/GridAdapter.java
@@ -0,0 +1,51 @@
+package com.ncumis.kendohamster;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class GridAdapter extends BaseAdapter {
+
+    Context context;
+    ArrayList<String> text = new ArrayList<>();
+
+    public GridAdapter(Context context, ArrayList<String> text) {
+        this.context = context;
+        this.text = text;
+    }
+
+    @Override
+    public int getCount() {
+        return text.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        @SuppressLint("ViewHolder") View v = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.motionlistgridview_layout, viewGroup, false);
+
+        TextView textView = v.findViewById(R.id.textView);
+        //assign data to the component
+        textView.setText(text.get(i));
+
+        return v;
+    }
+}
